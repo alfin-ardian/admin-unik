dd870a6a23511be643b63845c7c2afdb6bdfd457
alfin ardian <developer.alfin@gmail.com>
2023-09-02 18:27:40 +0700

    set up config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dotenv from "dotenv";
+import path from "path";
 
+const basePath = path.resolve(__dirname);
+dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@hooks": `${basePath}/src/hooks`,
+      "@helper": `${basePath}/src/helper`,
+      "@assets": `${basePath}/src/assets`,
+      "@router": `${basePath}/src/router`,
+      "@context": `${basePath}/src/context`,
+      types: `${basePath}/src/types`,
+      "@components": `${basePath}/src/components`,
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+  },
+});
