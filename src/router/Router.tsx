960e9ede1fdd96af9eeca9c981805b381d1d1c83
alfin ardian <developer.alfin@gmail.com>
2023-09-03 08:04:18 +0700

    adjust login page

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Login } from "../components/common/pages/auth/Login";
-import { Dashboard } from "../components/common/pages";
+import { Login, Dashboard } from "../components/common/pages";
 import { RootView } from "./RootView";
 
 export const router = createBrowserRouter([
