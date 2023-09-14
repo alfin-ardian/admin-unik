c18b75f9794e5e8b26517ec71fca5504e24ac05a
alfin ardian <developer.alfin@gmail.com>
2023-09-14 18:33:52 +0700

    adjust button

--- src/components/common/pages/dataAkun/calonTim/CalonTim.tsx
+++ src/components/common/pages/dataAkun/calonTim/CalonTim.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import { useGetTim } from "@hooks/api";
+import React, { useState } from "react";
 import { TimTable } from "@components/common/organisms";
 
 export const CalonTim: React.FC = () => {
@@ -11,14 +10,6 @@ export const CalonTim: React.FC = () => {
 
   return (
     <div className="container bg-white" style={{ borderRadius: "10px" }}>
-      <div className="flex justify-end p-4">
-        <Link
-          to={"/calon/tim-pernikahan/tambah"}
-          className="flex items-center gap-1 text-white bg-sky-600 hover:bg-sky-700 font-medium border border-sky-600 px-4 py-2 rounded-lg"
-        >
-          + Tambah
-        </Link>
-      </div>
       <div className="p-4">
         <TimTable kelompok={pengurus?.data} />
       </div>
